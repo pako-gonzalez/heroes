085b538959545ad1791587ab0cea0b747ebf3665
Francisco Jose Gonzalez Lozano <francisko.gonzalez@gmail.com>
2018-09-04 22:38:21 +0200

    commit inicial

---
+++ src/app/components/buscador/buscador.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute} from '@angular/router';
+import { HeroesService, Heroe } from '../../services/heroes.service';
+
+@Component({
+  selector: 'app-buscador',
+  templateUrl: './buscador.component.html',
+})
+export class BuscadorComponent implements OnInit {
+
+  heroes: Heroe[] = [];
+  termino: string;
+
+  constructor(private activatedRoute: ActivatedRoute,
+    private _heroesService: HeroesService) {
+  }
+
+  ngOnInit() {
+    this.activatedRoute.params.subscribe( params => {
+      this.termino = params['termino'];
+      this.heroes = this._heroesService.buscarHeroes(params['termino']);
+    });
+  }
+
+}
